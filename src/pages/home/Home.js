9997eb021eab3df2ccf18233b88940996147b45f
Mohammed Asaduzzaman Asad <maza21asad@gmail.com>
2024-07-28 22:29:01 +0600

    updated on both service and home pages

--- src/pages/home/Home.js
+++ src/pages/home/Home.js
@@ -2,9 +2,26 @@ import React from "react";
 import "./Home.css";
 import { NavLink } from "react-router-dom";
 
+import { Swiper, SwiperSlide } from "swiper/react";
+import "swiper/css";
+import "swiper/css/effect-coverflow";
+import "swiper/css/pagination";
+import "swiper/css/navigation";
+
+import {
+  EffectCoverflow,
+  Pagination,
+  Navigation,
+  Autoplay,
+} from "swiper/modules";
+import { IoArrowBack, IoArrowForward } from "react-icons/io5";
+// import { Parallax, ParallaxLayer } from "@react-spring/parallax";
+
 function Home() {
   return (
     <div className="home">
+      {/* <Parallax pages={1} className="animation"> */}
+      {/* <ParallaxLayer offset={0} speed={0.25}> */}
       <section id="intro">
         <div className="introContent">
           <span className="greeting">Hello, </span>
@@ -21,67 +38,195 @@ function Home() {
           </NavLink>
         </div>
         {/* <img src={bgImg} alt="bgImg" className="bgImg"></img> */}
-      </section>{" "}
-      <h4>Welcome to Empori BD</h4> <br />
-      <p>
-        Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo
-        ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis
-        dis parturient montes, nascetur ridiculus mus. Donec quam felis,
-        ultricies nec, pellentesque eu, pretium quis, sem. Nulla consequat massa
-        quis enim.
+      </section>
+      {/* </ParallaxLayer> */}
+      {/* ----------------- */}
+      {/* <ParallaxLayer offset={1} speed={0.25}> */}
+      <div className="secondIntro">
+        <div className="textSecondIntro">
+          {" "}
+          <br />
+          <br />
+          <h4>Welcome to Empori BD</h4>
+          <p>
+            Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean
+            commodo ligula eget dolor. Aenean massa.
+            <br />
+          </p>{" "}
+        </div>
+        <Swiper
+          effect={"coverflow"}
+          grabCursor={true}
+          centeredSlides={true}
+          loop={true}
+          slidesPerView={"auto"}
+          // coverflowEffect={{
+          //   rotate: 0,
+          //   stretch: 0,
+          //   depth: 100,
+          //   modifier: 2,
+          // }}
+          pagination={{ el: ".swiper-pagination", clickable: true }}
+          navigation={{
+            nextEl: ".swiper-button-next",
+            prevEl: ".swiper-button-prev",
+            clickable: true,
+          }}
+          autoplay={{
+            delay: 4500,
+            disableOnInteraction: false,
+          }}
+          modules={[EffectCoverflow, Pagination, Navigation, Autoplay]}
+          className="swiper-container"
+        >
+          <SwiperSlide>
+            <div className="swiperSlideContainer">
+              <NavLink to="/fashion&jewellery">
+                Empori Fashion & Jewellery
+              </NavLink>
+              <br />
+              <br />
+              Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean
+              commodo ligula eget dolor. Aenean massa. Cum sociis natoque
+              penatibus et magnis dis parturient montes, nascetur ridiculus mus.
+            </div>
+          </SwiperSlide>
+          <SwiperSlide>
+            <div className="swiperSlideContainer">
+              <p>Empori Properties Ltd</p>
+              <br />
+              <p>
+                Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean
+                commodo ligula eget dolor. Aenean massa. Cum sociis natoque
+                penatibus et magnis dis parturient montes, nascetur ridiculus
+                mus.
+              </p>
+            </div>
+          </SwiperSlide>
+          <SwiperSlide>
+            <div className="swiperSlideContainer">
+              <p>Empori Events</p>
+              <br />
+              <p>
+                Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean
+                commodo ligula eget dolor.
+              </p>
+            </div>
+          </SwiperSlide>
+          <SwiperSlide>
+            <div className="swiperSlideContainer">
+              <p>Empori Digitals</p> <br />
+              <p>
+                Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean
+                commodo ligula eget dolor. Aenean massa. Cum sociis natoque
+                penatibus et magnis dis parturient montes, nascetur ridiculus
+                mus.
+              </p>
+            </div>
+          </SwiperSlide>
+          <SwiperSlide>
+            <div className="swiperSlideContainer">
+              <p>Empori Tours & Travels</p> <br />
+              <p>
+                Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean
+                commodo ligula eget dolor. Aenean massa. Cum sociis natoque
+                penatibus et magnis dis parturient montes, nascetur ridiculus
+                mus.
+              </p>
+            </div>
+          </SwiperSlide>
+          <SwiperSlide>
+            <div className="swiperSlideContainer">
+              <p>Empori Architects</p> <br />
+              <p>
+                Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean
+                commodo ligula eget dolor. Aenean massa. Cum sociis natoque
+                penatibus et magnis dis parturient montes, nascetur ridiculus
+                mus.
+              </p>
+            </div>
+          </SwiperSlide>
+          <SwiperSlide>
+            <div className="swiperSlideContainer">
+              <p>Empori Agro</p> <br />
+              <p>
+                Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean
+                commodo ligula eget dolor. Aenean massa. Cum sociis natoque
+                penatibus et magnis dis parturient montes, nascetur ridiculus
+                mus.
+              </p>
+            </div>
+          </SwiperSlide>
+        </Swiper>
+        <div className="slider-controler">
+          <div className="swiper-button-prev slider-arrow">
+            <IoArrowBack name="arrow-back-outline"></IoArrowBack>
+          </div>
+          <div className="swiper-button-next slider-arrow">
+            <IoArrowForward className="arrow-forward-outline"></IoArrowForward>
+          </div>
+          <div className="swiper-pagination"></div>
+        </div>
+      </div>
+      {/* </ParallaxLayer> */}
+      {/* <ParallaxLayer sticky={{ start: 1, end: 2 }} /> */}
+      {/* ----------------------- */}
+      {/* <ParallaxLayer offset={2} speed={0.25}> */}
+      <div className="thirdIntro">
+        <br /> <br />
+        <p>Dealership Opportunities</p>
         <br />
-      </p>{" "}
-      <br /> <br />
-      <p>Dealership Opportunities</p>
-      <br />
-      <p>
-        Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo
-        ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis
-        dis parturient montes, nascetur ridiculus mus. Donec quam felis,
-        ultricies nec, pellentesque eu, pretium quis, sem. Nulla consequat massa
-        quis enim.
-      </p>
-      <br /> <br />
-      <p>Why Choose Us?</p>
-      <br />
-      <p>
-        * Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean
-        commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et
-        magnis dis parturient montes, nascetur ridiculus mus. Donec quam felis,
-        ultricies nec, pellentesque eu, pretium quis, sem. Nulla consequat massa
-        quis enim. <tr />
-        * Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean
-        commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et
-        magnis dis parturient montes, nascetur ridiculus mus. Donec quam felis,
-        ultricies nec, pellentesque eu, pretium quis, sem. Nulla consequat massa
-        quis enim. <tr />* Lorem ipsum dolor sit amet, consectetuer adipiscing
-        elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque
-        penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec
-        quam felis, ultricies nec, pellentesque eu, pretium quis, sem. Nulla
-        consequat massa quis enim. species.
-      </p>
-      <br /> <br />
-      <p>Our Mission</p>
-      <br />
-      <p>
-        Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo
-        ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis
-        dis parturient montes, nascetur ridiculus mus. Donec quam felis,
-        ultricies nec, pellentesque eu, pretium quis, sem. Nulla consequat massa
-        quis enim.
-      </p>
-      <br /> <br />
-      <p>Get to Know Us</p>
-      <br />
-      <p>
-        Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo
-        ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis
-        dis parturient montes, nascetur ridiculus mus. Donec quam felis,
-        ultricies nec, pellentesque eu, pretium quis, sem. Nulla consequat massa
-        quis enim.
-      </p>
-      <br /> <br />
-      <p>Thank you for visiting Empori BD.</p>
+        <p>
+          Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean
+          commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus
+          et magnis dis parturient montes, nascetur ridiculus mus. Donec quam
+          felis, ultricies nec, pellentesque eu, pretium quis, sem. Nulla
+          consequat massa quis enim.
+        </p>
+        <br /> <br />
+        <p>Why Choose Us?</p>
+        <br />
+        <p>
+          * Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean
+          commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus
+          et magnis dis parturient montes, nascetur ridiculus mus. Donec quam
+          felis, ultricies nec, pellentesque eu, pretium quis, sem. Nulla
+          consequat massa quis enim. <tr />
+          * Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean
+          commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus
+          et magnis dis parturient montes, nascetur ridiculus mus. Donec quam
+          felis, ultricies nec, pellentesque eu, pretium quis, sem. Nulla
+          consequat massa quis enim. <tr />* Lorem ipsum dolor sit amet,
+          consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean
+          massa. Cum sociis natoque penatibus et magnis dis parturient montes,
+          nascetur ridiculus mus. Donec quam felis, ultricies nec, pellentesque
+          eu, pretium quis, sem. Nulla consequat massa quis enim. species.
+        </p>
+        <br /> <br />
+        <p>Our Mission</p>
+        <br />
+        <p>
+          Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean
+          commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus
+          et magnis dis parturient montes, nascetur ridiculus mus. Donec quam
+          felis, ultricies nec, pellentesque eu, pretium quis, sem. Nulla
+          consequat massa quis enim.
+        </p>
+        <br /> <br />
+        <p>Get to Know Us</p>
+        <br />
+        <p>
+          Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean
+          commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus
+          et magnis dis parturient montes, nascetur ridiculus mus. Donec quam
+          felis, ultricies nec, pellentesque eu, pretium quis, sem. Nulla
+          consequat massa quis enim.
+        </p>
+        <br /> <br />
+        <p>Thank you for visiting Empori BD.</p>
+      </div>
+      {/* </ParallaxLayer> */}
+      {/* </Parallax> */}
     </div>
   );
 }
--- src/pages/services/Services.js
+++ src/pages/services/Services.js
@@ -1,150 +1,82 @@
 import React from "react";
 import "./Services.css";
-
-import { Swiper, SwiperSlide } from "swiper/react";
-import "swiper/css";
-import "swiper/css/effect-coverflow";
-import "swiper/css/pagination";
-import "swiper/css/navigation";
-
-import {
-  EffectCoverflow,
-  Pagination,
-  Navigation,
-  Autoplay,
-} from "swiper/modules";
-import { IoArrowBack, IoArrowForward } from "react-icons/io5";
 import { NavLink } from "react-router-dom";
 
 function Services() {
   return (
     <div className="services">
-      {" "}
-      <h4>Services</h4>
-      <p>
-        Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo
-        ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis
-        dis parturient montes, nascetur ridiculus mus. Donec quam felis,
-        ultricies nec, pellentesque eu, pretium quis, sem. Nulla consequat massa
-        quis enim.
-        <br />
-      </p>
-      {/* -------------------- */}
-      <Swiper
-        effect={"coverflow"}
-        grabCursor={true}
-        centeredSlides={true}
-        loop={true}
-        slidesPerView={"auto"}
-        coverflowEffect={{
-          rotate: 0,
-          stretch: 0,
-          depth: 100,
-          modifier: 2,
-        }}
-        pagination={{ el: ".swiper-pagination", clickable: true }}
-        navigation={{
-          nextEl: ".swiper-button-next",
-          prevEl: ".swiper-button-prev",
-          clickable: true,
-        }}
-        autoplay={{
-          delay: 4500,
-          disableOnInteraction: false,
-        }}
-        modules={[EffectCoverflow, Pagination, Navigation, Autoplay]}
-        className="swiper-container"
-      >
-        <SwiperSlide>
-          <div className="swiperSlideContainer">
-            <NavLink to="/fashion&jewellery">
-              Empori Fashion & Jewellery
-            </NavLink>
-            <br />
-            <br />
-            Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean
-            commodo ligula eget dolor. Aenean massa. Cum sociis natoque
-            penatibus et magnis dis parturient montes, nascetur ridiculus mus.
-          </div>
-        </SwiperSlide>
-        <SwiperSlide>
-          <div className="swiperSlideContainer">
-            <p>Empori Properties Ltd</p>
-            <br />
-            <p>
-              Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean
-              commodo ligula eget dolor. Aenean massa. Cum sociis natoque
-              penatibus et magnis dis parturient montes, nascetur ridiculus mus.
-            </p>
-          </div>
-        </SwiperSlide>
-        <SwiperSlide>
-          <div className="swiperSlideContainer">
-            <p>Empori Events</p>
-            <br />
-            <p>
-              Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean
-              commodo ligula eget dolor.
-            </p>
-          </div>
-        </SwiperSlide>
-        <SwiperSlide>
-          <div className="swiperSlideContainer">
-            <p>Empori Digitals</p> <br />
-            <p>
-              Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean
-              commodo ligula eget dolor. Aenean massa. Cum sociis natoque
-              penatibus et magnis dis parturient montes, nascetur ridiculus mus.
-            </p>
-          </div>
-        </SwiperSlide>
-        <SwiperSlide>
-          <div className="swiperSlideContainer">
-            <p>Empori Tours & Travels</p> <br />
-            <p>
-              Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean
-              commodo ligula eget dolor. Aenean massa. Cum sociis natoque
-              penatibus et magnis dis parturient montes, nascetur ridiculus mus.
-            </p>
-          </div>
-        </SwiperSlide>
-        <SwiperSlide>
-          <div className="swiperSlideContainer">
-            <p>Empori Architects</p> <br />
-            <p>
-              Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean
-              commodo ligula eget dolor. Aenean massa. Cum sociis natoque
-              penatibus et magnis dis parturient montes, nascetur ridiculus mus.
-            </p>
-          </div>
-        </SwiperSlide>
-        <SwiperSlide>
-          <div className="swiperSlideContainer">
-            <p>Empori Agro</p> <br />
-            <p>
-              Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean
-              commodo ligula eget dolor. Aenean massa. Cum sociis natoque
-              penatibus et magnis dis parturient montes, nascetur ridiculus mus.
-            </p>
-          </div>
-        </SwiperSlide>
-      </Swiper>
-      <div className="slider-controler">
-        <div className="swiper-button-prev slider-arrow">
-          <IoArrowBack name="arrow-back-outline"></IoArrowBack>
-        </div>
-        <div className="swiper-button-next slider-arrow">
-          <IoArrowForward className="arrow-forward-outline"></IoArrowForward>
-        </div>
-        <div className="swiper-pagination"></div>
-      </div>
-      {/* --------------------------- */}
-      <div>
+      <div className="serv1stDiv">
+        <h4 className="servH4">Services</h4>
         <p>
           Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean
           commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus
-          et magnis dis parturient montes, nascetur ridiculus mus.
+          et magnis dis parturient montes, nascetur ridiculus mus. Donec quam
+          felis, ultricies nec, pellentesque eu, pretium quis, sem. Nulla
+          consequat massa quis enim.
         </p>
+        <br />
+      </div>
+
+      <div className="servDiv">
+        <div className="serviceList">
+          <NavLink to="/fashion&jewellery" className="servDetails">
+            {/* <img className="servImg" /> */}
+            <h4 className="servName">EMPORI FASHION & JEWELLERY</h4>
+
+            <p className="servDesc">
+              Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean
+              commodo ligula eget dolor.
+            </p>
+          </NavLink>
+          <NavLink to="/propertiesLtd" className="servDetails">
+            {/* <img className="servImg" /> */}
+            <h4 className="servName">EMPORI PROPERTIES LTD</h4>
+            <p className="servDesc">
+              Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean
+              commodo ligula eget dolor.
+            </p>
+          </NavLink>
+          <NavLink to="/events" className="servDetails">
+            {/* <img className="servImg" /> */}
+            <h4 className="servName">EMPORI EVENTS</h4>
+            <p className="servDesc">
+              Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean
+              commodo ligula eget dolor.
+            </p>
+          </NavLink>
+          <NavLink to="/digitals" className="servDetails">
+            {/* <img className="servImg" /> */}
+            <h4 className="servName">EMPORI DIGITALS</h4>
+            <p className="servDesc">
+              Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean
+              commodo ligula eget dolor.
+            </p>
+          </NavLink>
+          <NavLink to="/tours&travels" className="servDetails">
+            {/* <img className="servImg" /> */}
+            <h4 className="servName">EMPORI TOURS & TRAVELS</h4>
+            <p className="servDesc">
+              Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean
+              commodo ligula eget dolor.
+            </p>
+          </NavLink>
+          <NavLink to="/architects" className="servDetails">
+            {/* <img className="servImg" /> */}
+            <h4 className="servName">EMPORI ARCHITECTS</h4>
+            <p className="servDesc">
+              Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean
+              commodo ligula eget dolor.
+            </p>
+          </NavLink>
+          <NavLink to="/agro" className="servDetails">
+            {/* <img className="servImg" /> */}
+            <h4 className="servName">EMPORI AGRO</h4>
+            <p className="servDesc">
+              Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean
+              commodo ligula eget dolor.
+            </p>
+          </NavLink>
+        </div>
       </div>
     </div>
   );
